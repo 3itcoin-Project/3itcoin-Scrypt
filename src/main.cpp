f828b6beefbc7a92acb1b33d53b0fbd05db0cf2c
3itcoin-Project <37435246+3itcoin-Project@users.noreply.github.com>
2018-05-07 19:49:32 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1103,7 +1103,7 @@ int64 static GetBlockValue(int nHeight, int64 nFees)
     return nSubsidy + nFees;
 }
 
-static const int64 nTargetTimespan = 1 * 2; // Threeitcoin: 2 seconds
+static const int64 nTargetTimespan = 1 * 15; // Threeitcoin: 15 seconds
 static const int64 nTargetSpacing = 1 * 1; // Threeitcoin: 1 second
 static const int64 nInterval = nTargetTimespan / nTargetSpacing;
 
